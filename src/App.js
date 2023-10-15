e34a3a9a92b3e335d37c7e503a8abedbd763ed1a
gireesh03 <gireeshnani03@gmail.com>
2023-10-15 13:03:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UserTable from "./UserTable.js";
+
+function App() {
+  return (
+    <div className="App">
+    <UserTable/>
+    </div>
+  );
+}
+
+export default App;
